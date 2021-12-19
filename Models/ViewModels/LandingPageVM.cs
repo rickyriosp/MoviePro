05f2e0ac79e563f910a137882541fccc66c9f01b
rickyriosp <riki.rios91@gmail.com>
2021-12-18 19:34:46 -0500

    landing view model

---
+++ Models/ViewModels/LandingPageVM.cs
@@ -0,0 +1,14 @@
+﻿using MovieProMVC.Models.Database;
+using MovieProMVC.Models.TMDB;
+
+namespace MovieProMVC.Models.ViewModels
+{
+    public class LandingPageVM
+    {
+        public List<Collection> CustomCollections { get; set; }
+        public MovieSearch NowPlaying { get; set; }
+        public MovieSearch Popular { get; set; }
+        public MovieSearch TopRated { get; set; }
+        public MovieSearch Upcoming { get; set; }
+    }
+}
