fc7f21ea43ab341683ceebf3a6d51084d8421345
rickyriosp <riki.rios91@gmail.com>
2021-12-18 22:30:04 -0500

    IDataMappingService

---
+++ Services/Interfaces/IDataMappingService.cs
@@ -0,0 +1,11 @@
+﻿using MovieProMVC.Models.Database;
+using MovieProMVC.Models.TMDB;
+
+namespace MovieProMVC.Services.Interfaces
+{
+    public interface IDataMappingService
+    {
+        Task<Movie> MapMovieDetailAsync(MovieDetail movie);
+        ActorDetail MapActorDetail(ActorDetail actor);
+    }
+}
