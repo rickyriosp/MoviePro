d2c3329225762691e5d49c4ab7760e207ac40420
rickyriosp <riki.rios91@gmail.com>
2021-12-18 19:25:23 -0500

    movie crew model

---
+++ Models/Database/MovieCrew.cs
@@ -0,0 +1,16 @@
+﻿namespace MovieProMVC.Models.Database
+{
+    public class MovieCrew
+    {
+        public int Id { get; set; }
+        public int MovieId { get; set; }
+
+        public int CrewId { get; set; }
+        public string Department { get; set; }
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public string ImageUrl { get; set; }
+
+        public Movie Movie { get; set; }
+    }
+}
