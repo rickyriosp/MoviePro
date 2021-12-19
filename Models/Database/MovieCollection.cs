faf31ace34856d7cb5d105db3baaedad6e250898
rickyriosp <riki.rios91@gmail.com>
2021-12-18 19:16:35 -0500

    movie collection model

---
+++ Models/Database/MovieCollection.cs
@@ -0,0 +1,14 @@
+﻿namespace MovieProMVC.Models.Database
+{
+    public class MovieCollection
+    {
+        public int Id { get; set; }
+        public int MovieId { get; set; }
+        public int CollectionId { get; set; }
+
+        public int Order { get; set; }
+        
+        public Movie Movie { get; set; }
+        public Collection Collection { get; set; }
+    }
+}
