ecf5bcfaff61cffbde1d094c408e9aab8a16ced6
rickyriosp <riki.rios91@gmail.com>
2021-12-18 22:25:52 -0500

    IRemoteMovieSevice

---
+++ Enums/MovieCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieProMVC.Enums
+{
+    public enum MovieCategory
+    {
+        now_playing,
+        popular,
+        top_rated,
+        upcoming
+    }
+}
---
+++ Services/Interfaces/IRemoteMovieService.cs
@@ -0,0 +1,12 @@
+﻿using MovieProMVC.Enums;
+using MovieProMVC.Models.TMDB;
+
+namespace MovieProMVC.Services.Interfaces
+{
+    public interface IRemoteMovieService
+    {
+        Task<MovieDetail> MovieDetailAsync(int id);
+        Task<MovieSearch> SearchMovieAsync(MovieCategory category, int count);
+        Task<ActorDetail> ActorDetailAsync(int id);
+    }
+}
