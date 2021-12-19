dfe72c3e13ba6c5504d64abe71231d53d4d5dcdf
rickyriosp <riki.rios91@gmail.com>
2021-12-18 19:22:46 -0500

    movie cast model

---
+++ Models/Database/MovieCast.cs
@@ -0,0 +1,16 @@
+﻿namespace MovieProMVC.Models.Database
+{
+    public class MovieCast
+    {
+        public int Id { get; set; }
+        public int MovieId { get; set; }
+
+        public int CastId { get; set; }
+        public string Department { get; set; }
+        public string Name { get; set; }
+        public string Character { get; set; }
+        public string ImageUrl { get; set; }
+
+        public Movie Movie { get; set; }
+    }
+}
