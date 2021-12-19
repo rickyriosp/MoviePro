1d61bdd4fcaa3ec6b7e9da887eff75d8bfd79837
rickyriosp <riki.rios91@gmail.com>
2021-12-18 23:02:06 -0500

    added IImageService

---
+++ Services/Interfaces/IImageService.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieProMVC.Services.Interfaces
+{
+    public interface IImageService
+    {
+        Task<byte[]> EncodeImageAsync(IFormFile poster);
+        Task<byte[]> EncodeImageUrlAsync(string imageUrl);
+        string DecodeImage(byte[] poster, string contentType);
+    }
+}
