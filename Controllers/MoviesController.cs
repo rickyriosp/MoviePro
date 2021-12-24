3179012706b2f5f4ecbedb6a997e4e73cec89559
rickyriosp <riki.rios91@gmail.com>
2021-12-24 15:56:14 -0500

    movie library action

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -58,6 +58,13 @@ namespace MovieProMVC.Controllers
             return RedirectToAction("Import");
         }
 
+        public async Task<IActionResult> Library()
+        {
+            var movies = await _context.Movie.ToListAsync();
+
+            return View(movies);
+        }
+
         private async Task AddToMovieCollection(int movieId, string collectionName)
         {
             var collection = await _context.Collection.FirstOrDefaultAsync(c => c.Name == collectionName);
