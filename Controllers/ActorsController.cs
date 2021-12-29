d1613c14fe346fdd0427e690318b3975aa7fc7be
rickyriosp <riki.rios91@gmail.com>
2021-12-28 20:13:05 -0500

    actors controller

---
+++ Controllers/ActorsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieProMVC.Services.Interfaces;
+
+namespace MovieProMVC.Controllers
+{
+    public class ActorsController : Controller
+    {
+        public readonly IRemoteMovieService _tmdbMovieService;
+        public readonly IDataMappingService _mappingService;
+
+        public ActorsController(IRemoteMovieService tmdbMovieService, IDataMappingService mappingService)
+        {
+            _tmdbMovieService = tmdbMovieService;
+            _mappingService = mappingService;
+        }
+
+        // GET: Actors/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            var actor = await _tmdbMovieService.ActorDetailAsync(id);
+            actor = _mappingService.MapActorDetail(actor);
+
+            return View(actor);
+        }
+    }
+}
