300c218784314627ef1920afe84f7ed513520f88
rickyriosp <riki.rios91@gmail.com>
2022-01-17 22:43:10 -0500

    created contact me view model

---
+++ Models/ViewModels/ContactMe.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieProMVC.Models.ViewModels
+{
+    public class ContactMe
+    {
+        [Required]
+        [StringLength(80, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters", MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email Address")]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters", MinimumLength = 2)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength (50, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters", MinimumLength = 2)]
+        public string Subject { get; set; }
+
+        [Required]
+        [StringLength(500, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters", MinimumLength = 2)]
+        public string Message { get; set; }
+    }
+}
